a6ec31fc66365056fc9e37dcccbe3cf8bce50aca
vchrisky <143357101+vchrisky@users.noreply.github.com.>
2024-05-31 17:08:28 +0300

    finished home page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+import onPageLoad from "./onPageLoad.js";
+import './style.css';
+
 console.log('We are up and Running');
+
+window.addEventListener('load', () => {
+    // onPageLoad();
+});
--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,31 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  // mode: 'development',
   plugins: [
     new HtmlWebpackPlugin({
         template: './src/index.html',
         title: 'Parsed',
     })
   ],
+  devtool: 'inline-source-map',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  devServer: {
+    historyApiFallback: true
   },
 };
